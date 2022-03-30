d1010baa8225bf99114a85cec21bb9897e5a9614
solodescargasmio <solodescargasmio@gmail.com>
2022-03-30 12:21:50 -0300

    Primer commit Back

---
+++ src/test/java/co/com/sofka/ferreteria/FerreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.ferreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
